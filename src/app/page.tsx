d1805b142bf24b04928314522c393ba8fc314352
savitar47 <jonasmaidana47@gmail.com>
2025-10-04 15:38:58 -0400

    Adding not-found.tsx page, and loading.tsx page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import StoreNavigation from "@/app/pages/store/store-navigation.page";
+import {redirect} from "next/navigation";
 
 export default function Home() {
-    return (
-        <StoreNavigation></StoreNavigation>
-    );
+    redirect('/store');
 }
