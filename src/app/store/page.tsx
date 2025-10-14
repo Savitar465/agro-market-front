6382dc5951eeef72572e10c3749a853f680d6970
Savitar <jonasmaidana47@gmail.com>
2025-10-14 08:56:36 -0400

    Adding product details page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -6,7 +6,7 @@ export default function ProductsFeed(){
 
                 <div
                     className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    <a href="#" className="group">
+                    <a href="/products/1234956" className="group">
                         <img
                             src="https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg"
                             alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
